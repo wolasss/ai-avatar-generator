eebcebf7c44b394802750841a8270444fdceb4de
Adam Wolski <wolas.abc@gmail.com>
2023-01-19 18:24:32 +0100

    feat: new prompts hints

--- config.js
+++ config.js
@@ -1,29 +1,31 @@
 export default {
     hints: [
         {
-            label: 'a russian spy',
+            label: 'a cyberpunk',
             prompts: [
-                'Photo of Adam in a dark alley, Realism, wearing a long trench coat and a fedora hat with the Russian flag embroidered on it, 4k, highly detailed',
-                'Photo of Adam standing in the shadows of a Moscow skyline, Impressionism, with a fur coat and a red star lapel pin, 4k, highly detailed',
-                'Photo of Adam in a secret meeting, Surrealism, with a briefcase handcuffed to his arm and a grim expression on his face, 4k, highly detailed',
-                'Photo of Adam climbing a ladder to a rooftop escape, Pointillism, with a balaclava covering his face and a backpack full of documents, 4k, highly detailed',
-                'Photo of Adam infiltrating a building, Expressionism, with a silenced pistol in one hand and a dossier in the other, 4k, highly detailed',
-                'Photo of Adam holding a silenced rifle, Photorealism, in a dark alley with a high- rise building in the background, 4k, highly detailed',
-                'Photo of Adam in a secure server room, Cubism, with a USB drive in one hand and a laptop in the other, 4k, highly detailed',
-                'Photo of Adam in the back of a car, Abstract Expressionism, with an earpiece in one ear and a walkie- talkie in the other, 4k, highly detailed'
+                'Closeup portrait of Adam as a cyberpunk, dark and gritty, highly detailed, retro-futuristic style, neon lighting, cyberpunk city in the background, art by wlop, greg rutkowski, and charlie bowater, 8k resolution, ultra-realistic, octane render, unreal engine.',
+                'Closeup portrait of Adam in a cyberpunk style, wearing a neon-lit leather jacket, with retro-futuristic goggles and a cyberpunk city in the background, art by Wlop, Greg Rutkowski, and Charlie Bowater, 8k resolution, ultra-realistic, Octane Render, Unreal Engine.',
+                'Closeup portrait of Adam with a cyberpunk-inspired mohawk, neon-lit cyberpunk armor, and a retro-futuristic cyberpunk cityscape in the background, art by Wlop, Greg Rutkowski, and Charlie Bowater, 8k resolution, ultra-realistic, Octane Render, Unreal Engine.',
+                'Closeup portrait of Adam in a cyberpunk style, neon-lit cybernetic implants, and a detailed cyberpunk cityscape in the background, art by Wlop, Greg Rutkowski, and Charlie Bowater, 8k resolution, ultra-realistic, Octane Render, Unreal Engine.',
+                'Closeup portrait of Adam with a cyberpunk-inspired tattoo, wearing a neon-lit cyberpunk mask, and a gritty cyberpunk cityscape in the background, art by Wlop, Greg Rutkowski, and Charlie Bowater'
             ]
         },
         {
             label: 'a star wars character',
             prompts: [
+                'closeup portrait of Adam as a jedi with a lightsaber, highly detailed, science fiction, star wars concept art, intricate details, bright colors, golden hour, art by marko djurdjevic, greg rutkowski, wlop, fredperry, digital painting, rossdraws.',
+                'Closeup portrait of Adam as a Jedi with a lightsaber, highly detailed, science fiction, Star Wars concept art, intricate details, bright and vibrant colors, gold and silver accents, art by Marko Djurdjevic, Greg Rutkowski, WLOP, Fred Perry, digital painting, Rossdraws.',
+                'Closeup portrait of Adam as a Jedi with a lightsaber and a cape, highly detailed, science fiction, Star Wars concept art, intricate details, bright and vivid colors, golden hour, art by Marko Djurdjevic, Greg Rutkowski, WLOP, Fred Perry, digital painting, Rossdraws.',
+                'Closeup portrait of Adam as a Jedi with a lightsaber and a hood, highly detailed, science fiction, Star Wars concept art, intricate details, bright and vibrant colors, golden hour, art by Marko Djurdjevic, Greg Rutkowski, WLOP, Fred Perry, digital painting, Rossdraws.',
+                'Closeup portrait of Adam as a Jedi with a lightsaber and a hood, highly detailed, science fiction, Star Wars concept art, intricate details, dark and mysterious colors, golden hour, art by Marko Djurdjevic',
                 'Digital Art, Adam in a Jedi Robe, Realistic, with a blue lightsaber.',
                 'Digital Art, Adam as a Sith Lord, Semi - Realistic, with a red lightsaber, 4k',
                 'Digital illustration of Adam as an Imperial Officer, Impressionistic, with a black cape, 4k',
                 'Digital Art, Adam as an X- Wing Pilot, Semi - Realistic, with a helmet and blaster, 4k',
-                'portrait of Adam as a Rebel Alliance Member, Semi- Abstract, with a blaster, 4k',
-                'Digital Art, Adam as a Wookiee, Semi - Realistic, with a bowcaster and fur, 4k',
-                'Digital Art, Adam as a Stormtrooper, Realistic, with a helmet and blaster rifle, 4k',
-                'Watercolor Painting, Adam as a Bounty Hunter, Impressionistic, with a jetpack and blaster, 4k',
+                'Portrait of Adam as a Rebel Alliance Member, Semi- Abstract, with a blaster, 4k',
+                'Photo of Adam as a Wookiee, Semi - Realistic, with a bowcaster and fur, 4k',
+                'Photo of Adam as a Stormtrooper, Realistic, with a helmet and blaster rifle, 4k',
+                'Photo of Adam as a Bounty Hunter, Impressionistic, with a jetpack and blaster, 4k',
                 'Digital Art, Adam as a Hutt Gangster, Semi - Realistic, with a blaster pistol and a cigar, 4k',
                 'Digital Art, Adam as a Sith Apprentice, Semi - Realistic, with a red lightsaber and hooded robe, 4k'
             ] 
@@ -41,6 +43,7 @@ export default {
         {
             label: 'a king',
             prompts: [
+                'Adam as young elven king long white hair beautiful face highly detailed face, portrait, made by stanley artgerm lau, wlop, rossdraws, james jean, andrei riabovitchev, krenz kushart, marc simonetti, yoshitaka amano, artstation',
                 'Digital Illustration, Adam seated on a throne, Hyperrealism, Adam wearing a golden crown and a long red cape, 4k',
                 'Digital Illustration, Adam being coronated by two angels, Surrealism, Adam wearing a white and blue robe with a long flowing cape, 4k',
                 'Digital Illustration, Adam walking on the streets of his kingdom, Fantasy, Adam wearing a gold armor and a large feathered cape, 4k',
@@ -52,25 +55,27 @@ export default {
             ]
         },
         {
-            label: 'a santa claus',
+            label: 'an astronaut',
             prompts: [
-                'Digital Painting, Adam wearing a Santa suit, Realistic, With a white fluffy beard and a sack full of presents, 4k',
-                'Photo of Adam dressed as Santa, Impressionistic, With a big smile on his face, 4k',
-                'Portrait of Adam in a Santa hat, Abstract, With a toy in the background, 4k',
-                'Portrait of Adam with a Santa beard, Expressionist, With a twinkle in his eye, 4k',
-                'Portrait of Adam with a Santa hat and coat, Pop- art, With a blurred background of Christmas lights, 4k',
-                'Digital Art, Adam in a Santa suit, Minimalist, With a single snowflake in the corner, 4k',
-                'Photo of Adam holding a sack of gifts, Cartoon - style, With a rosy - red nose and cheeks, 4k',
-                'Portrait of Adam wearing a Santa hat and coat, Surreal, With a reindeer in the background, 4k',
-                'Portrait of Adam dressed as Santa, Cubist, With a festive wreath in the background, 4k',
-                'Portrait of Sculpture, Adam in a Santa suit, Realistic, With a bag full of presents in one hand, 4k'
+                'closeup portrait of Adam as an astronaut, futuristic, highly detailed, ultra realistic, concept art, intricate textures, interstellar background, space travel, art by alphonse mucha, ryan kittleson, greg rutkowski, leesha hannigan, stephan martiniere, stanley artgerm lau.',
+                'Closeup portrait of Adam as an astronaut, futuristic, with a retro-futuristic steampunk design, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut in a sleek, modern, and minimalistic style, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut with a comic book-style, vibrant colour palette, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut with a surreal, dreamlike effect, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut in a highly stylized, abstract art style, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut with an ethereal, eerily beautiful aesthetic, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut, featuring a celestial, star-filled background, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut, with a vintage, Art Deco inspired design, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut, with a whimsical, psychedelic feel, ultra realistic, 8k',
+                'Closeup portrait of Adam as an astronaut, with a sci-fi, neon-lit futuristic atmosphere, ultra realistic, 8k'
             ]
         },
         {
             label: 'a soldier',
             prompts: [
+                'Portrait of Adam as a victorian general wearing military uniform, male, detailed face, victorian, highly detailed, cinematic lighting, digital art painting by greg rutkowski',
                 'Photo of Adam in full military gear, Photorealism, Wearing a helmet and carrying a rifle, 4k, highly detailed',
-                'Photo of Adam saluting, Impressionism, Wearing a helmet and camouflage, 4k, highly detailed',
+                'Closeup portrait of Adam as a military general, ultra realistic, 8k, highly detailed',
                 'Photo of Adam firing a gun, Surrealism, Wearing a helmet and face paint, 4k, highly detailed',
                 'Photo of Adam on patrol, Realism, Wearing a helmet and carrying a backpack, 4k, highly detailed',
                 'Photo of Adam in formation, Abstract, Wearing a helmet and marching with other soldiers, 4k, highly detailed',
@@ -84,6 +89,18 @@ export default {
         {
             label: 'something random',
             prompts: [
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'closeup portrait of Adam as a superhero, dynamic lighting, intense colors, detailed costume, artstation trending, art by alphonse mucha, greg rutkowski, ross tran, leesha hannigan, ignacio fernandez rios, kai carpenter, noir photorealism, film',
+                'Closeup portrait of Adam as a Hobbit, small, big brown eyes, green and brown clothing, detailed facial features, small feet, wispy hair, fantasy concept art, artstation trending, highly detailed, art by John Howe, Alan Lee, and Weta Workshop, earthy colors, looking into camera.',
+                'closeup portrait painting of Adam as a viking, ultra realistic, concept art, intricate details, powerful and fierce, highly detailed, photorealistic, octane render, 8 k, unreal engine. art by artgerm and greg rutkowski and charlie bowater and magali villeneuve and alphonse mucha, golden hour, horns and braids in hair, fur-lined cape and helmet, axe in hand, looking towards the camera.',
+                'Portrait of Adam by Greg Rutkowski, he is about 25 years old, british, young, attractive, slim, hardened but somehow friendly look, he is wearing futuristic military fatigues, highly detailed portrait, scifi, digital painting, artstation, concept art, smooth, sharp foccus ilustration, Artstation HQ',
+                'Adam prince :: by Martine Johanna and Simon Stålenhag and Chie Yoshii and Casey Weldon and wlop :: ornate, dynamic, particulate, rich colors, intricate, elegant, highly detailed, centered, artstation, smooth, sharp focus, octane render, 3d',
+                'close up of face of Adam fashion model in white feather clothes, official balmain editorial, dramatic lighting highly detailed',
                 'Photo of Adam with a determined expression in a Black and White Film Noir style, with a dramatic low- key lighting, 4k, highly detailed',
                 'Photo of Adam with a deep thoughtful gaze in a High- key lighting style, with a subtle vintage filter, 4k, highly detailed',
                 'Photo of Adam with a head tilt and a slight smirk in a Moody Dark style, with a textured background, 4k, highly detailed',
